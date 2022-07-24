22136ab8a089a8bd51613af45e4ca5367e595aa6
crocha12 <camilorocha11@gmail.com>
2022-07-23 23:35:02 -0300

    add code

--- desafio/forca.js
+++ desafio/forca.js
@@ -1,14 +1,54 @@
 class Forca {
 
-  chutar(letra) { }
+  constructor(palavraForca) {
+    this.palavraForca = palavraForca;
+    this.vidas = 6;
+    this.letrasChutadas = [];
+    this.palavra = []
+    for(let i = 0; i < palavraForca.length; i++) {
+      this.palavra.push("_")
+    }
+  }
 
-  buscarEstado() { return ""; } // Possiveis valores: "perdeu", "aguardando chute" ou "ganhou"
+  checarPalavra() {
+    return !this.palavra.includes("_");
+  }
+
+  adicionarLetraEmPalavra(letra) {
+    for (let i in this.palavraForca) {
+      if (this.palavraForca[i] === letra) {
+        this.palavra[i] = letra;
+      }
+    }
+  }
+
+  chutar(letra) { 
+    if (letra.length === 1) {
+      if (!this.letrasChutadas.includes(letra)) {
+        if (this.palavraForca.includes(letra)) {
+          this.adicionarLetraEmPalavra(letra)
+        } else {
+          this.vidas = this.vidas - 1
+        }
+        this.letrasChutadas.push(letra)
+      }
+    }
+  }
+
+  buscarEstado() { 
+    if (this.vidas == 0) {
+      return "perdeu"
+    } else if (this.checarPalavra()) {
+      return "ganhou"
+    }
+    return "aguardando chute";
+   } // Possiveis valores: "perdeu", "aguardando chute" ou "ganhou"
 
   buscarDadosDoJogo() {
       return {
-          letrasChutadas: [], // Deve conter todas as letras chutadas
-          vidas: 6, // Quantidade de vidas restantes
-          palavra: [] // Deve ser um array com as letras que já foram acertadas ou o valor "_" para as letras não identificadas
+          letrasChutadas: this.letrasChutadas, // Deve conter todas as letras chutadas
+          vidas: this.vidas, // Quantidade de vidas restantes
+          palavra: this.palavra // Deve ser um array com as letras que já foram acertadas ou o valor "_" para as letras não identificadas
       }
   }
 }
